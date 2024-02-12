9638ecd7748bcca2ff8a4fc82050cf8f0ca55e2f
RelativeBinary <patrick.bornay@gmail.com>
2024-02-12 21:31:53 +1100

    add basic validation

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -6,29 +6,54 @@ type FormValues = {
   username: string;
   email: string;
   channel: string;
-}
+};
 
 export const YouTubeForm = () => {
   const form = useForm<FormValues>();
   const { register, control, handleSubmit } = form;
 
   const onSubmit = (data: FormValues) => {
-    console.log('submitted', data);
-  }
+    console.log("submitted", data);
+  };
 
   return (
     <div>
       <h1>YouTube Form</h1>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" {...register("username")} />
+        <input
+          type="text"
+          id="username"
+          {...register("username", {
+            required: {
+              value: true,
+              message: "Username is required",
+            },
+          })}
+        />
 
         <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" {...register("email")} />
+        <input
+          type="email"
+          id="email"
+          {...register("email", {
+            pattern: {
+              value:
+                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              message: "Invalid email format",
+            },
+          })}
+        />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" {...register("channel")} />
+        <input
+          type="text"
+          id="channel"
+          {...register("channel", {
+            required: { value: true, message: "Username is required" },
+          })}
+        />
 
         <button>Submit</button>
       </form>
