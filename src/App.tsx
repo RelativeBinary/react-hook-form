9d7f41750d2db1f0f86b4549b04bbc7df82ab052
RelativeBinary <patrick.bornay@gmail.com>
2024-02-21 21:04:12 +1100

    yup example

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { YouTubeForm } from './components/YouTubeForm'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { YouTubeForm } from "./components/YouTubeForm";
+import { YupYouTubeForm } from "./components/YupYoutubeForm";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-       <YouTubeForm />
+        <YouTubeForm />
+        <br />
+        <YupYouTubeForm />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
