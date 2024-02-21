9d9d260f59a50cc21870e1fc209561ffc5087949
RelativeBinary <patrick.bornay@gmail.com>
2024-02-22 06:14:13 +1100

    add dev tools

--- src/components/ContextConsumingForm.tsx
+++ src/components/ContextConsumingForm.tsx
@@ -1,3 +1,4 @@
+import { DevTool } from "@hookform/devtools";
 import React from "react";
 import { useForm } from "react-hook-form";
 
@@ -7,26 +8,24 @@ const defaultValues: FormValues = {
 
 const ContextConsumingForm = (): React.JSX.Element => {
   const form = useForm<FormValues>({ defaultValues });
-	const { register, formState } = form;
-	const { errors } = formState;
+  const { register, formState, control } = form;
+  const { errors } = formState;
   return (
     <form>
+      <DevTool control={control} />
       <h1>Context consuming form</h1>
-      <label htmlFor="username"></label>
-      <div>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          {...register("username", {
-            required: {
-              value: true,
-              message: "Username is required",
-            },
-          })}
-        />
-        <p className="error">{errors.username?.message}</p>
-      </div>
+      <label htmlFor="username">Username</label>
+      <input
+        type="text"
+        id="username"
+        {...register("username", {
+          required: {
+            value: true,
+            message: "Username is required",
+          },
+        })}
+      />
+      <p className="error">{errors.username?.message}</p>
     </form>
   );
 };
@@ -40,7 +39,7 @@ export interface FormValues {
 /** TODO
  * - [x] Introduce RHF
  * - [ ] Create context and provide it (App.tsx level)
- * - [ ] Consume context and interact with form  (This component)
- * - [ ] Add yup validation 
+ * - [ ] Consume context via hook and interact with form (This component)
+ * - [ ] Add yup validation
  * - [ ] Add more fields (Similar to YouTubeForm.tsx)
  */
